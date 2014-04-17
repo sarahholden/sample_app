7d5232ed55f9f65e89af7638847d525443546fc2
Sarah Holden <sarahbeth.holden@gmail.com>
2014-04-16 20:44:34 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,7 +35,7 @@ describe "Static pages" do
 		end
 
 		it "should have the right title" do
-			visit 'static_pages/about'
+			visit '/static_pages/about'
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
